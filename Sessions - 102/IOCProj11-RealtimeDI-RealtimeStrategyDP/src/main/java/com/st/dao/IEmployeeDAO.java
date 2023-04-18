7e780bb4f306ca227cd2b27cbac824bfaf1cc840
Shivam Vishwakarma <arvind.972176@gmail.com>
2023-04-18 11:41:55 +0530

    Realtime Dependency Injection using MySql

---
+++ Sessions - 102/IOCProj11-RealtimeDI-RealtimeStrategyDP/src/main/java/com/st/dao/IEmployeeDAO.java
@@ -0,0 +1,7 @@
+package com.st.dao;
+
+import com.st.bo.EmployeeBO;
+
+public interface IEmployeeDAO {
+   public int  insertEmployee(EmployeeBO bo)throws Exception;
+}
---
+++ Sessions - 102/IOCProj11-RealtimeDI-RealtimeStrategyDP/src/main/java/com/st/service/IEmployeeMgmtService.java
@@ -0,0 +1,7 @@
+package com.st.service;
+
+import com.st.dto.EmployeeDTO;
+
+public interface IEmployeeMgmtService {
+    public  String registerEmployee(EmployeeDTO dto)throws Exception;
+}
